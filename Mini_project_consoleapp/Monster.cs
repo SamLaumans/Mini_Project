b54140cc32fad65a623ffb1eacdf563a5a38f112
Nickraaihr <1081708@hr.nl>
2024-02-26 14:24:11 +0100

    monster.cs aangemaakt en monster stats aangepast

---
+++ Mini_project_consoleapp/Monster.cs
@@ -0,0 +1,17 @@
+public class Monster
+{
+    public int Id ;
+    public string Name ;
+    public int Health ;
+    public int Attack ;
+    public int Defense ;
+
+    public Monster(int id, string name, int health, int attack, int defense)
+    {
+        Id = id;
+        Name = name;
+        Health = health;
+        Attack = attack;
+        Defense = defense;
+    }
+}
--- Mini_project_consoleapp/World.cs
+++ Mini_project_consoleapp/World.cs
@@ -45,13 +45,13 @@ public static class World
 
     public static void PopulateMonsters()
     {
-        Monster rat = new Monster(MONSTER_ID_RAT, "rat", 1, 3, 3);
+        Monster rat = new Monster(MONSTER_ID_RAT, "rat", 1, 3, 1);
 
 
-        Monster snake = new Monster(MONSTER_ID_SNAKE, "snake", 10, 7, 7);
+        Monster snake = new Monster(MONSTER_ID_SNAKE, "snake", 5, 10, 3);
 
 
-        Monster giantSpider = new Monster(MONSTER_ID_GIANT_SPIDER, "giant spider", 3, 10, 10);
+        Monster giantSpider = new Monster(MONSTER_ID_GIANT_SPIDER, "giant spider", 10, 5, 10);
 
 
         Monsters.Add(rat);
