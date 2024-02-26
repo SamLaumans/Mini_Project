1e3f03851a390af8dfe537080fe51893cfbcb4dc
Jorio27 <1080277@hr.nl>
2024-02-26 14:36:38 +0100

    Quest class en program.cs gecleared

---
+++ Mini_project_consoleapp/Quest.cs
@@ -0,0 +1,14 @@
+public class Quest
+{
+    public int Id ;
+    public string Name ;
+    public string Description ;
+
+
+    public Quest(int id, string name, string description)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+    }
+}
