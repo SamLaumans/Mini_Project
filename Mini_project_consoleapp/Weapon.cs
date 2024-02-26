143f0fa11f31c2fd57ea38fad94f9839cedaba2f
Nickraaihr <1081708@hr.nl>
2024-02-26 14:52:03 +0100

    Hallo kameraden , ik heb bij deze de weapon class aangemaakt , met vriendelijke groet Nick

---
+++ Mini_project_consoleapp/Weapon.cs
@@ -0,0 +1,13 @@
+public class Weapon
+{
+    public int Id;
+    public string Name;
+    public int Attack;
+
+    public Monster(int id, string name, int Attack)
+    {
+        Id = id;
+        Name = name;
+        Attack = Attack;
+    }
+}
